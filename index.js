a3e60dff2f11660a5150964601e83bde4a18424c
Your Nameclear <BrianA>
2025-12-07 16:51:20 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+require("datejs");
+
+function combineUsers(...args) {
+ 
+  const combinedObject = {
+    users: []
+  };
+
+  for (const group of args) {
+    combinedObject.users.push(...group);
+  }
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+module.exports = combineUsers;
 
 
 
